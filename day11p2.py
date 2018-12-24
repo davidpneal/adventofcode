3838aa74b2c29a11895e365e686c2d4306cb1bec
David Neal <35005080+davidpneal@users.noreply.github.com>
2018-12-24 14:31:32 -0600

    Advent of Code Day 11 Puzzle 2
    This solution works but can probably still be improved.  It takes several seconds to run and the method used to bound the calcAreaPower parameters may not work correctly when the solution is on the edge of the matrix.

--- day11p2.py
+++ day11p2.py
@@ -1,8 +1,13 @@
-#12/23/2018
+#12/24/2018
 #Find the square which has the largest total power, the square can be anywhere from 1x1 to 300x300
 
+#The package numpy has some tools that can help with the multidimensional arrays and creating the summed area table
+#Note that numpy uses matrix indexing (i,j / row,col) vs cartesian indexing (x,y) --> if the matrix is printed out, it will be "rotated"
+import numpy as np
+
+
 #Puzzle input
-serialNum = 18
+serialNum = 9995
 
 
 
@@ -17,55 +22,62 @@ def calcCellPower(x,y,serialNum):
 	powerLevel = hundreds - 5
 
 	return powerLevel
+
+
+
+def calcAreaPower(x,y,s):
+	#Need to bound these params - if we are on the edge (ie, 0) will index outside the table!
+	#This method will probably cause issues if the solution is near the edge of the grid, but works for the problem here
+	if x == 0:
+		x = 1
 	
+	if y == 0:
+		y = 1
 	
-
-def calcGridPower(size):
-	max = 0
-	for x in range(300-size):
-		for y in range (300-size):
-		
-			sum = 0
-			for sumx in range(x,x+size):
-				for sumy in range(y,y+size):
-					sum += fuelGrid[sumx][sumy]
-
-			if sum > max:
-				max = sum
-				loc = x+1,y+1
-	
-	return x,y,sum
+	#Must subtract 1 from the size (s) since the grid size is inclusive; ie, if the grid is 3x3, adding 3 would check a grid that is 4x4
+	power = sumTable[x+(s-1)][y+(s-1)] + sumTable[x-1][y-1] - sumTable[x-1][y+(s-1)] - sumTable[x+(s-1)][y-1]
+	return power	
 
 
 
-#Create fuel grid: 300x300
-fuelGrid = [([0] * 300) for x in range(300)]
+#Create fuel grid: 300x300, use ints (defaults to float)
+fuelGrid = np.zeros(shape=(300,300),dtype=int)
 
 #Populate the values in the fuelGrid
 for x in range(300):
 	for y in range(300):
 		fuelGrid[x][y] = calcCellPower(x+1, y+1, serialNum)
 
-		
-#Find the cell with the highest power rating, the cell is identified by the x,y coordinate in the upper left corner
+#Calculate summed area table
+sumTable = fuelGrid.cumsum(axis=0).cumsum(axis=1)
+
+
+#Find the square with the highest power rating, it is identified by the x,y coordinate in the upper left corner
 max = 0
 for s in range(299):
-	print(s)
-	best = calcGridPower(s)
-	
-	if best[2] > max:
-		max = best[2]
-		loc = best[0]+1,best[1]+1,s
-
-			
+	for x in range(300-s):
+		for y in range(300-s):
+			sum = calcAreaPower(x,y,s)
+			if sum > max:
+				max = sum
+				#Add one to the answer since the matrix starts from 0
+				loc = x+1,y+1,s
+				##print("new max:",max,loc)
+		
 print("Largest total power:",loc)
 print("Total power:",max)
 
 
+
 ''' MISC
 
-Correct answer: 
+Correct answer: 233,116,15 
 
 
+#print a partial grid
+for x in range(10):
+	print(fuelGrid[x][:10])
+	
+	
 	
 '''
